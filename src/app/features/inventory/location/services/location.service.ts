a513cf3342957255f0a980c12962d3b3337f4e69
juan jose <juanjosenarvaezmarin13092005@gmail.com>
2026-06-06 00:27:43 -0500

    agregando service

--- src/app/features/inventory/location/services/location.service.ts
+++ src/app/features/inventory/location/services/location.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { EnvironmentService } from '@core/services/environment.service';
 import { Observable } from 'rxjs';
-import { LocationPatchRequest, LocationRequest, LocationResponse } from '../models/location.model';
+import {
+  LocationPatchRequest,
+  LocationRequest,
+  LocationResponse
+} from '../models/location.model';
 
 @Injectable({ providedIn: 'root' })
 export class LocationService {
-  private readonly baseUrl = '/inventory/api/v1/location';
+  private readonly apiRoot: string;
+  private readonly baseUrl: string;
 
-  constructor(private readonly http: HttpClient) {}
+  constructor(
+    private readonly http: HttpClient,
+    private readonly env: EnvironmentService
+  ) {
+    this.apiRoot = this.env.apiUrl.replace(/\/api\/v1\/?$/, '');
+    this.baseUrl = `${this.apiRoot}/inventory/api/v1/location`;
+  }
 
   list(): Observable<LocationResponse[]> {
     return this.http.get<LocationResponse[]>(this.baseUrl);
